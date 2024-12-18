b4eaea2b52df10921cc5d0d114d731d97ad08a77
Swathi M <swathim14102000@gmail.com>
2024-12-18 19:56:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,45 @@
+
+import tkinter as tk
 import requests
-api_key = 'fa16d168ab7c1526f974ba9eab0dc575'
-user_input = input("Enter city:")
-weather_data = requests.get(
-    f"https://api.openweathermap.org/data/2.5/weather?q={user_input}&units=imperial&APPID={api_key}")
-if weather_data.json()['cod']=='404':
-    print("No City Found")
-else:
-    weather =weather_data.json()['weather'][0]['main']
-    temp = round(weather_data.json()['main']['temp'])
-    print(f"The weather in {user_input} is :{weather}")
-    print(f"The temperature in {user_input} is:{temp}ºF")
+import time
+ 
+
+def getWeather(canvas):
+    city = textField.get()
+    api = "https://api.openweathermap.org/data/2.5/weather?q="+city+"&appid=fa16d168ab7c1526f974ba9eab0dc575"
+    
+    json_data = requests.get(api).json()
+    condition = json_data['weather'][0]['main']
+    temp = int(json_data['main']['temp'] - 273.15)
+    min_temp = int(json_data['main']['temp_min'] - 273.15)
+    max_temp = int(json_data['main']['temp_max'] - 273.15)
+    pressure = json_data['main']['pressure']
+    humidity = json_data['main']['humidity']
+    wind = json_data['wind']['speed']
+    sunrise = time.strftime('%I:%M:%S', time.gmtime(json_data['sys']['sunrise'] - 21600))
+    sunset = time.strftime('%I:%M:%S', time.gmtime(json_data['sys']['sunset'] - 21600))
+
+    final_info = condition + "\n" + str(temp) + "°C" 
+    final_data = "\n"+ "Min Temp: " + str(min_temp) + "°C" + "\n" + "Max Temp: " + str(max_temp) + "°C" +"\n" + "Pressure: " + str(pressure) + "\n" +"Humidity: " + str(humidity) + "\n" +"Wind Speed: " + str(wind) + "\n" + "Sunrise: " + sunrise + "\n" + "Sunset: " + sunset
+    label1.config(text = final_info)
+    label2.config(text = final_data)
+
+
+canvas = tk.Tk()
+canvas.geometry("600x500")
+canvas.title("Weather App")
+f = ("poppins", 15, "bold")
+t = ("poppins", 35, "bold")
+
+textField = tk.Entry(canvas, justify='center', width = 20, font = t)
+textField.pack(pady = 20)
+textField.focus()
+textField.bind('<Return>', getWeather)
+
+label1 = tk.Label(canvas, font=t)
+label1.pack()
+label2 = tk.Label(canvas, font=f)
+label2.pack()
+canvas.mainloop()
+
 
